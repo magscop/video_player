7a6fc3647d0574c56363f929511dc2d58bdf366f
magscop <107082508+magscop@users.noreply.github.com>
2024-04-07 14:20:18 -0400

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Maggie Copland on 2024-04-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
